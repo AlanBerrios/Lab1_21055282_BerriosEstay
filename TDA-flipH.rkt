0eecf149cfa2f1984354215ca149851e32e37f9b
AlanBerrios <alan.berrios@usach.cl>
2022-09-15 16:25:25 -0300

    TDA-flipH update
    Se corrige el flipH, se definen anonimamente flopH y mymaprecur y se realiza de forma recursiva con la funcion mymaprecur

--- TDA-flipH.rkt
+++ TDA-flipH.rkt
@@ -4,26 +4,41 @@
 
 ; TDA modificador
 
-#| Documentacion:  flopH
-Descripcion: Funcion que permite modificar el valor de la posicion x del pixel recibido
+#| Documentacion: flopH
+Descripcion: Funcion que permite modificar el valor de la posicion x del pixel, se encuentra definida dentro de flipH
 Tipo de algoritmo/estrategia: No aplica
 Dominio(Argumento de entrada): ancho(int) X  pix(pixbit-d|pixrgb-d|pixhex-d) 
 Recorrido(Retorno): Booleano
 |#
-
-(define flopH (lambda (ancho pix)
-                (append (list (list-ref pix 0) (abs (- (list-ref pix 1) (- ancho 1)))) (cddr pix))))
                          
 #| Documentacion:  TDA-flipH
 Descripcion: Funcion que modifica la posicion de los pixeles de la imagen en la coordenada x
 Tipo de algoritmo/estrategia: No aplica
+Tipo de recursion: De cola (mediante la funcion mymaprecur)
 Dominio(Argumento de entrada): lista (image) 
 Recorrido(Retorno): lista (image) flipeada
 |#
 
+#| Documentacion: mymaprecur
+Descripcion: Version recursiva de la funcion map, se encuentra definida dentro del flipH
+Tipo de algoritmo/estrategia: No aplica
+Tipo de recursion: De cola
+Dominio(Argumento de entrada): procedimiento (funcion) X lista (list) 
+Recorrido(Retorno): lista (list)
+|#
+
 (define flipH (lambda (lista)
+                
+                (define mymaprecur (lambda (procedimiento lista)
+                                     (if (null? lista)
+                                         null
+                                         (cons (procedimiento (car lista)) (mymaprecur procedimiento (cdr lista))))))
+                
+                (define flopH (lambda (ancho pix)
+                                (append (list (list-ref pix 0) (abs (- (list-ref pix 1) (- ancho 1)))) (cddr pix))))
+                
                 (if (and (list? lista)(not(null? lista)))
-                    (list (list-ref lista 0)(list-ref lista 1)(map (lambda (p) (flopH (list-ref lista 1) p)) (list-ref lista 2)))
+                    (list (list-ref lista 0)(list-ref lista 1)(mymaprecur (lambda (p) (flopH (list-ref lista 1) p)) (list-ref lista 2)))
                     null)))
 
 (provide (all-defined-out))
